606516801e83efa40cdf3435f2121e19341b72db
Gamila <gamilaaah@gmail.com>
2019-05-09 01:36:41 +0200

    FilterDesign_filteration is ready for GUI

--- FilterDesign_filteration.m
+++ FilterDesign_filteration.m
@@ -5,8 +5,9 @@ signal= xlsread([filename,file]);
 plot(signal)
 
 %insertion of Fsampling from gui and mapping it from 0 to pi
-Fsampling=150; % elmafrod byegy ka input mn el gui
+Fsampling=150; % elmafrod byegy ka input mn el gui mn editline
 Frequency= linspace(0,Fsampling./2,315);
+
 %unit circle vector sabet fe kolo (manual and built-in)
 theta = 0:0.01:pi;
 theta=transpose(theta);
@@ -15,20 +16,40 @@ x=z_coordinate.*sin(theta);
 y=z_coordinate.*cos(theta);
 z_coordinate=x+y*i;
 % dol el 2 arrays ely  gayeen mn goz2 eman 
-zeros=[1]; % lazem column vector 3shan zp2tf
-poles=[0];
+zeros=[1 ; .7+i]; % lazem column vector 3shan zp2tf
+poles=[1;.5];
 %get tf from from zeros and poles matlab
-[num_coeff,den_coeff]=zp2tf(zeros,poles,1);
+[num_coeff,den_coeff]=zp2tf(zeros,poles,1); % 1 dah kan k 
+%mn awel hena da el built-in gain
 Polynomial_tf= tf(num_coeff,den_coeff);
 for k =1:length(z_coordinate)
-   gain_matlab(k)=evalfr(Polynomial_tf,z_coordinate(k));
-   gain_matlab=gain_matlab(:);
+   substitution_in_tf(k)=evalfr(Polynomial_tf,z_coordinate(k));
+   substitution_in_tf=substitution_in_tf(:);
+   gain_matlab=20*log(substitution_in_tf); % to get mag of tf
 end
 figure
 plot (Frequency,gain_matlab)
 
 
-% filter functio sbta fe kolo 
+
+%hena dah el manual gain
+distance=ones(315,1);
+for i=1:length(z_coordinate)
+    for j=1:length(zeros)        
+      distance(i)= distance(i)*norm(z_coordinate(i)- zeros(j));  
+    end
+end
+
+for i=1:length(z_coordinate)
+    for j=1:length(poles)       
+      distance(i)= distance(i)*(1./norm(z_coordinate(i)- poles(j)));
+    end
+end
+manual_gain=20*log(distance);
+figure 
+plot(Frequency, manual_gain);
+
+% filter function sbta fe kolo
 filtered_signal = filter(num_coeff,den_coeff,signal) ;
 figure 
 plot(filtered_signal)
